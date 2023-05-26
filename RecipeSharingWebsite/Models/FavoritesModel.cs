dceb2e8b70a056dbcb0d4139499a39bbd2063240
aleynahasagdas <aleyna.hasagdas@boun.edu.tr>
2023-05-26 13:02:50 +0300

    Initial commit

---
+++ RecipeSharingWebsite/Models/FavoritesModel.cs
@@ -0,0 +1,8 @@
+namespace RecipeAleyna.Models;
+
+public class FavoritesModel
+{
+    public int FavoritesId { get; set; }
+    public int UserId { get; set; }
+    public int RecipeId { get; set; }
+}
---
+++ RecipeSharingWebsite/Models/RatingsModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeAleyna.Models;
+
+public class RatingsModel
+{
+    public int RatingId { get; set; }
+    public int RecipeId { get; set; }
+    public int UserId { get; set; }
+    public int Rating { get; set; }
+}
